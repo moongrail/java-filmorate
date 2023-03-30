b42802c294655e36ffac0e17fef211c957a8d7ec
Moongrail <omgone666@yandex.ru>
2023-03-30 06:17:46 +0300

    style: fixed annotations

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.validate.ValidLoginUser;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
